986a1b2a6f26f1de90a7280c91448bef96f9bbae
kamitoshi <kroo.aq.74@gmail.com>
2020-06-20 09:11:17 +0900

    modify user DB and form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :kana_name, :sex, :birthday, :phone_number, :email, :introduction, :image)
+    params.require(:user).permit(:name, :kana_name, :sex, :birthday, :phone_number, :prefecture, :city, :street, :building, :email, :introduction, :image)
   end
 
   def set_user
